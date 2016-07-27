930722ae7504fc470da4b71f24dd3992ec321207
Kevin Bost <kevin@intellitect.com>
2016-07-27 09:38:39 -0700

    Added CastleWindsor example

---
+++ Examples/CastleWindsorExample/CastleWindsorExample.cs
@@ -0,0 +1,33 @@
+﻿using AutoDI;
+using Castle.MicroKernel.Registration;
+using Castle.Windsor;
+using ExampleClasses;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CastleWindsorExample
+{
+    [TestClass]
+    public class CastleWindsorExample
+    {
+        [TestMethod]
+        public void CanUseCastleWindsor()
+        {
+            using ( var container = new WindsorContainer() )
+            {
+                container.Register(Component.For<IService>().ImplementedBy<Service>());
+                try
+                {
+                    DependencyResolver.Set( new CastleWindsorResolver(container) );
+
+                    var @class = new Class();
+
+                    Assert.IsTrue( @class.Service is Service );
+                }
+                finally
+                {
+                    DependencyResolver.Set( (IDependencyResolver)null );
+                }
+            }
+        }
+    }
+}
