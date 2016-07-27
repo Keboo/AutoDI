1d9a9e6bb0ac422ac94dbca913f290db49c15ce6
Kevin Bost <kevin@intellitect.com>
2016-07-27 10:21:52 -0700

    Adding Spring.Net example

---
+++ Examples/SprintNet.Example/SprintNetExample.cs
@@ -0,0 +1,36 @@
+﻿using AutoDI;
+using ExampleClasses;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Spring.Context.Support;
+using Spring.Objects;
+
+namespace SprintNet.Example
+{
+    [TestClass]
+    public class SprintNetExample
+    {
+        [TestMethod]
+        public void CanUseSprintNet()
+        {
+            using ( var context = new StaticApplicationContext() )
+            {
+                context.RegisterPrototype(typeof(IService).FullName, typeof(Service), new MutablePropertyValues());
+
+                try
+                {
+                    DependencyResolver.Set( new SprintNetResolver( context ) );
+
+                    var @class = new Class();
+
+                    Assert.IsTrue( @class.Service is Service );
+                }
+                finally
+                {
+                    DependencyResolver.Set( (IDependencyResolver)null );
+                }
+            }
+        }
+    }
+
+
+}
