ecb53dda38fb016134ca2f65f3220cc8620e7e21
Kevin B <kitokeboo@gmail.com>
2017-06-25 22:25:21 -0700

    Adding dot net core surrot. Lots of refactoring to support it.

---
+++ AutoDI.Fody_orig/Injector.cs
@@ -0,0 +1,78 @@
+﻿using Mono.Cecil;
+using Mono.Cecil.Cil;
+using System;
+
+namespace AutoDI.Fody
+{
+    internal class Injector
+    {
+        private readonly MethodDefinition _constructor;
+        private int _insertionPoint;
+
+        public Injector(MethodDefinition constructor)
+        {
+            _constructor = constructor ?? throw new ArgumentNullException(nameof(constructor));
+        }
+
+        public void Insert(OpCode code, TypeReference type)
+        {
+            Insert(Instruction.Create(code, type));
+        }
+
+        public void Insert(OpCode code, MethodReference method)
+        {
+            Insert(Instruction.Create(code, method));
+        }
+
+        public void Insert(OpCode code, int value)
+        {
+            Insert(Instruction.Create(code, value));
+        }
+
+        public void Insert(OpCode code)
+        {
+            Insert(Instruction.Create(code));
+        }
+
+        public void Insert(OpCode code, VariableDefinition variable)
+        {
+            Insert(Instruction.Create(code, variable));
+        }
+
+        public void Insert(OpCode code, Instruction target)
+        {
+            Insert(Instruction.Create(code, target));
+        }
+
+        public void Insert(OpCode code, ParameterDefinition parameter)
+        {
+            Insert(Instruction.Create(code, parameter));
+        }
+
+        public void Insert(OpCode code, string value)
+        {
+            Insert(Instruction.Create(code, value));
+        }
+
+        public void Insert(OpCode code, long value)
+        {
+            Insert(Instruction.Create(code, value));
+        }
+
+        public void Insert(OpCode code, double value)
+        {
+            Insert(Instruction.Create(code, value));
+        }
+
+        public void Insert(OpCode code, float value)
+        {
+            Insert(Instruction.Create(code, value));
+        }
+
+        public void Insert(Instruction instruction)
+        {
+            _constructor.Body.Instructions.Insert(_insertionPoint++, instruction);
+        }
+
+    }
+}
--- Examples/AutoDI.Container/Console.NetCore/Program.cs
+++ Examples/AutoDI.Container/Console.NetCore/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using AutoDI;
+using AutoDI.Container.Examples;
 
 namespace Console.NetCore
 {
@@ -6,7 +8,30 @@ namespace Console.NetCore
     {
         static void Main(string[] args)
         {
+            var quoteBoard = new QuoteBoard();
+            quoteBoard.ShowQuotes();
+            System.Console.ReadLine();
+        }
+    }
 
+    public class QuoteBoard
+    {
+        private readonly IQuoteService _service;
+
+        public QuoteBoard([Dependency]IQuoteService service = null)
+        {
+            _service = service;
+            if (service == null) throw new ArgumentNullException(nameof(service));
+        }
+
+        public void ShowQuotes()
+        {
+            foreach (Quote quote in _service.GetQuotes())
+            {
+                System.Console.WriteLine($"{quote.Text}");
+                System.Console.WriteLine($"   -{quote.Author}");
+                System.Console.WriteLine();
+            }
         }
     }
 }
