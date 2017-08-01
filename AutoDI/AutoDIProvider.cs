d59bfcea06cbc4b17a02284a9698f11f758bd59b
Kevin B <kitokeboo@gmail.com>
2017-07-31 23:11:51 -0700

    Just messing around wiht the abstraction interfaces

---
+++ AutoDI/AutoDIProvider.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AutoDI
+{
+    public class AutoDIServiceProvider : IServiceProvider, ISupportRequiredService
+    {
+        public AutoDIServiceProvider(ContainerMap containerBuilder)
+        {
+            
+        }
+
+        public object GetService(Type serviceType)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object GetRequiredService(Type serviceType)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class AutoDIServiceScope : IServiceScopeFactory
+    {
+        public IServiceScope CreateScope()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class AutoDIServiceProviderFactory : IServiceProviderFactory<ContainerMap>
+    {
+        public ContainerMap CreateBuilder(IServiceCollection services)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IServiceProvider CreateServiceProvider(ContainerMap containerBuilder)
+        {
+            return new AutoDIServiceProvider(containerBuilder);
+        }
+    }
+}
