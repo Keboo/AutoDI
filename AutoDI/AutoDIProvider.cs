7c5e115fb1ff4d9edb27e35f0252b7e5ba62deaa
Kevin B <kitokeboo@gmail.com>
2017-08-10 23:47:03 -0700

    Work on abstractions

--- AutoDI/AutoDIProvider.cs
+++ AutoDI/AutoDIProvider.cs
@@ -1,13 +1,13 @@
-﻿using System;
-using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
 
 namespace AutoDI
 {
     public class AutoDIServiceProvider : IServiceProvider, ISupportRequiredService
     {
-        public AutoDIServiceProvider(ContainerMap containerBuilder)
+        public AutoDIServiceProvider(ContainerMap map)
         {
-            
         }
 
         public object GetService(Type serviceType)
@@ -33,12 +33,113 @@ namespace AutoDI
     {
         public ContainerMap CreateBuilder(IServiceCollection services)
         {
-            throw new NotImplementedException();
+            var map = new ContainerMap();
+
+            Lifetime GetLifetime(ServiceDescriptor descriptor)
+            {
+                if (descriptor is AutoDIServiceDescriptor autoDiDescriptor)
+                {
+                    return autoDiDescriptor.Lifetime;
+                }
+                //TODO: Translate
+                return Lifetime.Transient;
+            }
+
+            foreach (ServiceDescriptor serviceDescriptor in services)
+            {
+                switch (GetLifetime(serviceDescriptor))
+                {
+                    case Lifetime.Singleton:
+                        map.AddSingleton();
+                        break;
+                    case Lifetime.LazySingleton:
+
+                        break;
+                    case Lifetime.Transient:
+
+                        break;
+                }
+            }
+
+            return map;
         }
 
         public IServiceProvider CreateServiceProvider(ContainerMap containerBuilder)
         {
             return new AutoDIServiceProvider(containerBuilder);
+        }
+    }
+
+    public class AutoDIServiceScopeFactory : IServiceScopeFactory
+    {
+        public IServiceScope CreateScope()
+        {
+            throw new NotImplementedException();
+        }
+
+        private class AutoDIServiceScope : IServiceScope
+        {
+            public void Dispose()
+            {
+                throw new NotImplementedException();
+            }
+
+            public IServiceProvider ServiceProvider { get; }
+        }
+    }
+
+    public class AutoDIServiceCollection : List<ServiceDescriptor>, IServiceCollection
+    {
+
+    }
+
+    public class AutoDIServiceDescriptor : ServiceDescriptor
+    {
+        public Lifetime Lifetime { get; }
+
+        public AutoDIServiceDescriptor(Type serviceType, Type implementationType, ServiceLifetime lifetime) : base(serviceType, implementationType, lifetime)
+        {
+        }
+
+        public AutoDIServiceDescriptor(Type serviceType, object instance) : base(serviceType, instance)
+        {
+        }
+
+        public AutoDIServiceDescriptor(Type serviceType, Func<IServiceProvider, object> factory, ServiceLifetime lifetime) : base(serviceType, factory, lifetime)
+        {
+        }
+    }
+
+    public static class AutoDI
+    {
+        private static AutoDIServiceProvider _serviceProvider;
+
+        public static void Init(Action<IServiceCollection> configureMethod)
+        {
+            var collection = new AutoDIServiceCollection();
+            configureMethod(collection);
+
+            _serviceProvider = new AutoDIServiceProvider(collection);
+        }
+
+        private static void Gen_Configured(AutoDIServiceCollection collection)
+        {
+            //TODO: All of the adds....
+        }
+    }
+
+    public class P
+    {
+        public static void Main()
+        {
+
+
+
+        }
+
+        public static void Configure(IServiceCollection serviceCollection)
+        {
+
         }
     }
 }
