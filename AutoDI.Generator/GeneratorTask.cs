f462af708e28bda5339c25046f133a6490a50f7c
Kevin B <kitokeboo@gmail.com>
2018-03-12 22:52:03 -0700

    Better indenting and adding support for DebugExceptions.

--- AutoDI.Generator/GeneratorTask.cs
+++ AutoDI.Generator/GeneratorTask.cs
@@ -33,6 +33,7 @@ namespace AutoDI.Generator
 
         public override bool Execute()
         {
+            
             XElement configElement = GetConfigElement(ProjectPath);
             var settings = Settings.Parse(new Settings(), configElement);
             var logger = new TaskLogger(BuildEngine, settings.DebugLogLevel);
@@ -50,7 +51,7 @@ namespace AutoDI.Generator
                 Directory.CreateDirectory(Path.GetDirectoryName(GeneratedFilePath));
                 using (var file = File.Open(GeneratedFilePath, FileMode.Create))
                 {
-                    WriteClass(mapping, SetupMethod.Find(compiledAssembly.MainModule, logger), file);
+                    WriteClass(mapping, settings, SetupMethod.Find(compiledAssembly.MainModule, logger), file);
                     GeneratedCodeFiles = new ITaskItem[] { new TaskItem(GeneratedFilePath) };
                 }
             }
@@ -70,17 +71,19 @@ namespace AutoDI.Generator
             return null;
         }
 
-        private static void WriteClass(Mapping mapping, MethodDefinition setupMethod, Stream output)
+        private static void WriteClass(Mapping mapping, Settings settings, MethodDefinition setupMethod, Stream output)
         {
+            const string @namespace = "AutoDI.Generated";
+            const string className = "AutoDI";
             using (var sw = new StreamWriter(output))
             {
-                sw.WriteLine("using System;");
-                sw.WriteLine("using AutoDI;");
-                sw.WriteLine("using Microsoft.Extensions.DependencyInjection;");
-                sw.WriteLine("namespace AutoDI.Generated");
-                sw.WriteLine("{");
-                sw.WriteLine("    public static partial class AutoDI");
-                sw.WriteLine("    {");
+                sw.WriteLine(0, "using System;");
+                sw.WriteLine(0, "using AutoDI;");
+                sw.WriteLine(0, "using Microsoft.Extensions.DependencyInjection;");
+                sw.WriteLine(0, $"namespace {@namespace}");
+                sw.WriteLine(0, "{");
+                sw.WriteLine(1, $"public static partial class {className}");
+                sw.WriteLine(1, "{");
 
                 int index = 0;
                 foreach (TypeMap typeMap in mapping)
@@ -89,74 +92,101 @@ namespace AutoDI.Generator
                     MethodDefinition ctor = typeMap.TargetType.GetMappingConstructor();
                     if (ctor == null) continue;
 
-                    sw.WriteLine($"        private static global::{typeMap.TargetType.FullNameCSharp()} generated_{index++}(IServiceProvider serviceProvider)");
-                    sw.WriteLine("        {");
-                    sw.Write($"            return new global::{typeMap.TargetType.FullNameCSharp()}(");
+                    sw.WriteLine(2, $"private static global::{typeMap.TargetType.FullNameCSharp()} generated_{index++}(IServiceProvider serviceProvider)");
+                    sw.WriteLine(2, "{");
+                    sw.Write(3, $"return new global::{typeMap.TargetType.FullNameCSharp()}(");
 
                     sw.Write(string.Join(", ", ctor.Parameters.Select(p => $"serviceProvider.GetService<global::{p.ParameterType.FullNameCSharp()}>()")));
 
                     sw.WriteLine(");");
-                    sw.WriteLine("        }");
+                    sw.WriteLine(2, "}");
                 }
 
-                sw.WriteLine("        private static void AddServices(IServiceCollection collection)");
-                sw.WriteLine("        {");
+                sw.WriteLine(2, "private static void AddServices(IServiceCollection collection)");
+                sw.WriteLine(2, "{");
+                if (settings.DebugExceptions)
+                {
+                    sw.WriteLine(3, "List<Exception> list = new List<Exception>();");
+                }
                 index = 0;
                 foreach (TypeMap typeMap in mapping)
                 {
                     if (!typeMap.TargetType.CanMapType() || typeMap.TargetType.GetMappingConstructor() == null) continue;
+                    
                     foreach (TypeLifetime lifetime in typeMap.Lifetimes)
                     {
-                        sw.WriteLine($"            collection.AddAutoDIService<global::{typeMap.TargetType.FullNameCSharp()}>(generated_{index}, new System.Type[{lifetime.Keys.Count}]");
-                        sw.WriteLine("            {");
-                        sw.Write("                ");
-                        sw.WriteLine(string.Join(", ", lifetime.Keys.Select(t => $"typeof(global::{t.FullNameCSharp()})")));
-                        sw.WriteLine($"            }}, Lifetime.{lifetime.Lifetime});");
+                        int indent = 3;
+                        if (settings.DebugExceptions)
+                        {
+                            sw.WriteLine(indent, "try");
+                            sw.WriteLine(indent++, "{");
+                        }
+
+                        sw.WriteLine(indent, $"collection.AddAutoDIService<global::{typeMap.TargetType.FullNameCSharp()}>(generated_{index}, new System.Type[{lifetime.Keys.Count}]");
+                        sw.WriteLine(indent, "{");
+                        sw.WriteLine(indent + 1, string.Join(", ", lifetime.Keys.Select(t => $"typeof(global::{t.FullNameCSharp()})")));
+                        sw.WriteLine(indent, $"}}, Lifetime.{lifetime.Lifetime});");
+
+                        if (settings.DebugExceptions)
+                        {
+                            sw.WriteLine(--indent, "}");
+                            sw.WriteLine(indent, "catch(Exception innerException)");
+                            sw.WriteLine(indent, "{");
+                            sw.WriteLine(indent + 1, $"list.Add(new AutoDIException(\"Error adding type '{typeMap.TargetType.FullNameCSharp()}' with key(s) '{string.Join(",", lifetime.Keys.Select(x => x.FullName))}'\", innerException));");
+                            sw.WriteLine(indent, "}");
+                        }
                     }
+                    
                     index++;
                 }
-                sw.WriteLine("        }");
 
-                sw.WriteLine("        static partial void DoInit(Action<IApplicationBuilder> configure)");
-                sw.WriteLine("        {");
-                sw.WriteLine("            if (_globalServiceProvider != null)");
-                sw.WriteLine("            {");
-                sw.WriteLine("                throw new AlreadyInitializedException();");
-                sw.WriteLine("            }");
-                sw.WriteLine("            IApplicationBuilder applicationBuilder = new ApplicationBuilder();");
-                sw.WriteLine("            applicationBuilder.ConfigureServices(AddServices);");
+                if (settings.DebugExceptions)
+                {
+                    sw.WriteLine(3, "if (list.Count > 0)");
+                    sw.WriteLine(3, "{");
+                    sw.WriteLine(4, $"throw new AggregateException(\"Error in {@namespace}.{className}.AddServices() generated method\", list);");
+                    sw.WriteLine(3, "}");
+                }
+                sw.WriteLine(2, "}");
+
+                sw.WriteLine(2, "static partial void DoInit(Action<IApplicationBuilder> configure)");
+                sw.WriteLine(2, "{");
+                sw.WriteLine(3, "if (_globalServiceProvider != null)");
+                sw.WriteLine(3, "{");
+                sw.WriteLine(4, "throw new AlreadyInitializedException();");
+                sw.WriteLine(3, "}");
+                sw.WriteLine(3, "IApplicationBuilder applicationBuilder = new ApplicationBuilder();");
+                sw.WriteLine(3, "applicationBuilder.ConfigureServices(AddServices);");
                 if (setupMethod != null)
                 {
-                    sw.WriteLine($"            global::{setupMethod.DeclaringType.FullNameCSharp()}.{setupMethod.Name}(applicationBuilder);");
+                    sw.WriteLine(3, $"global::{setupMethod.DeclaringType.FullNameCSharp()}.{setupMethod.Name}(applicationBuilder);");
                 }
-                sw.WriteLine("            if (configure != null)");
-                sw.WriteLine("            {");
-                sw.WriteLine("                configure(applicationBuilder);");
-                sw.WriteLine("            }");
-                sw.WriteLine("            _globalServiceProvider = applicationBuilder.Build();");
-                sw.WriteLine("            GlobalDI.Register(_globalServiceProvider);");
-                sw.WriteLine("        }");
+                sw.WriteLine(3, "if (configure != null)");
+                sw.WriteLine(3, "{");
+                sw.WriteLine(4, "configure(applicationBuilder);");
+                sw.WriteLine(3, "}");
+                sw.WriteLine(3, "_globalServiceProvider = applicationBuilder.Build();");
+                sw.WriteLine(3, "GlobalDI.Register(_globalServiceProvider);");
+                sw.WriteLine(2, "}");
 
-                sw.WriteLine("        static partial void DoDispose()");
-                sw.WriteLine("        {");
-                sw.WriteLine("            IDisposable disposable;");
-                sw.WriteLine("            if ((disposable = (_globalServiceProvider as IDisposable)) != null)");
-                sw.WriteLine("            {");
-                sw.WriteLine("                disposable.Dispose();");
-                sw.WriteLine("            }");
-                sw.WriteLine("            GlobalDI.Unregister(_globalServiceProvider);");
-                sw.WriteLine("            _globalServiceProvider = null;");
-                sw.WriteLine("        }");
+                sw.WriteLine(2, "static partial void DoDispose()");
+                sw.WriteLine(2, "{");
+                sw.WriteLine(3, "IDisposable disposable;");
+                sw.WriteLine(3, "if ((disposable = (_globalServiceProvider as IDisposable)) != null)");
+                sw.WriteLine(3, "{");
+                sw.WriteLine(4, "disposable.Dispose();");
+                sw.WriteLine(3, "}");
+                sw.WriteLine(3, "GlobalDI.Unregister(_globalServiceProvider);");
+                sw.WriteLine(3, "_globalServiceProvider = null;");
+                sw.WriteLine(2, "}");
 
 
-                sw.WriteLine("        private static IServiceProvider _globalServiceProvider;");
+                sw.WriteLine(2, "private static IServiceProvider _globalServiceProvider;");
 
-                sw.WriteLine("    }");
-                sw.WriteLine("}");
+                sw.WriteLine(1, "}");
+                sw.WriteLine(0, "}");
             }
         }
-
-
 
         public void Cancel()
         {
